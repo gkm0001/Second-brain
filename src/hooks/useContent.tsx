6c288f94df8cd2b46fdb3158e647fb3f5762d0aa
GOVIND Mishra <professionalgkm@gmail.com>
2025-02-26 22:05:57 +0530

    Add error handling in api

--- src/hooks/useContent.tsx
+++ src/hooks/useContent.tsx
@@ -1,29 +1,37 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
- export function useContent(){
-     const [contents , setContents] = useState([]);
+export function useContent() {
+  const [contents, setContents] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string|null>(null);
 
-     async function refersh(){
-         await axios.get(`${import.meta.env.VITE_BACKEND_URL}api/v1/content/allcontent`, {
-               headers : {
-                    "Authorization":localStorage.getItem("token")
-               }
-              }).then((res)=>{
-                    setContents(res.data.content)
-                })
-     }
-
-     useEffect(()=>{
-        refersh()
-        let interval = setInterval(()=>{
-           refersh()
-        },10*1000)
-       
-
-        return() => {
-           clearInterval(interval)
+  async function refresh() {
+    try {
+      setLoading(true);
+      const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}api/v1/content/allcontent`, {
+        headers: {
+          "Authorization": localStorage.getItem("token")
         }
-     },[])
-     return { contents,refersh}
+      });
+      setContents(res.data.content);
+    } catch (err) {
+      setError("Failed to load content");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    refresh();
+    const interval = setInterval(() => {
+      refresh();
+    }, 10 * 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return { contents, refresh, loading, error };
 }
